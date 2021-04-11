4268e8ee45aa1cf20c73ec20328f8009462789c3
Volodymyr Udovychenko <vosap2007@i.ua>
2021-04-11 09:31:05 +0300

    2

--- contacts.js
+++ contacts.js
@@ -1,6 +1,5 @@
 const fs = require('fs/promises')
 const path = require('path')
-
 //const contactsPath = path.join('db', 'contacts.json')
 const contactsPath = path.join(__dirname, './bd/contacts.json')
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const contacts = ('./contacts')
+const contacts = require('./contacts')
 const { Command } = require('commander')
 const program = new Command()
 program
